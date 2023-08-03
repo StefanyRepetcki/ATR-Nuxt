0328b4cf3ab0218cc44f5d2cbcd8a959c6860363
Stefany Repetcki <stefany.c.r@hotmail.com>
2023-08-03 15:06:38 -0300

    feat: SEO meta tags

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,31 +1,33 @@
 export default {
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    title: 'nuxt',
+    title: "nuxt",
     htmlAttrs: {
-      lang: 'en'
+      lang: "pt-BR",
     },
     meta: [
-      { charset: 'utf-8' },
-      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: '' },
-      { name: 'format-detection', content: 'telephone=no' }
+      { charset: "utf-8" },
+      { name: "viewport", content: "width=device-width, initial-scale=1" },
+      // hid é o identificador de cada meta tag
+      { hid: "description", name: "description", content: "Descrição SEO" },
+      { name: "format-detection", content: "telephone=no" },
     ],
-    link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
-    ]
+    link: [{ rel: "icon", type: "image/x-icon", href: "/favicon.ico" }],
+    script: [
+      {
+        src: "https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.29.4/moment.min.js",
+      },
+    ],
   },
 
   // Global CSS: https://go.nuxtjs.dev/config-css
-  css: [
-  ],
+  css: [],
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
-  plugins: [
-  ],
+  plugins: [],
 
   router: {
-    middleware: 'auth',
+    middleware: "auth",
   },
 
   // Auto import components: https://go.nuxtjs.dev/config-components
@@ -34,22 +36,21 @@ export default {
   // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
   buildModules: [
     // https://go.nuxtjs.dev/tailwindcss
-    '@nuxtjs/tailwindcss',
+    "@nuxtjs/tailwindcss",
   ],
 
   // Modules: https://go.nuxtjs.dev/config-modules
   modules: [
     // https://go.nuxtjs.dev/axios
-    '@nuxtjs/axios',
+    "@nuxtjs/axios",
   ],
 
   // Axios module configuration: https://go.nuxtjs.dev/config-axios
   axios: {
     // Workaround to avoid enforcing hard-coded localhost:3000: https://github.com/nuxt-community/axios-module/issues/308
-    baseURL: '/',
+    baseURL: "/",
   },
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
-  build: {
-  }
-}
+  build: {},
+};
--- pages/servicos.vue
+++ pages/servicos.vue
@@ -2,27 +2,57 @@
   <div>
     <h1 class="text-lg font-bold">Serviços</h1>
     <NuxtLink to="/servicos/desenvolvimento-de-sites"
-      >Desenvolvimento de sites</NuxtLink
-    >
+      >Desenvolvimento de sites
+    </NuxtLink>
     <NuxtLink to="/servicos/marketing-digital">Marketing Digital</NuxtLink>
     <br /><br />
     <div v-if="$fetchState.pending">Carregando...</div>
-
-    <div v-for="service in services" :key="service.id" class="border-b border-gray-400 py-4">{{ service.username }}</div>
+    <div
+      v-for="service in services"
+      :key="service.id"
+      class="border-b border-gray-400 py-4"
+    >
+      {{ service.username }}
+    </div>
 
     <NuxtChild />
   </div>
 </template>
 <script>
 export default {
+  head() {
+    return {
+        title: this.title,
+        meta: [
+          { hid: "description", name: "description", content: "Descrição pagina serviços" },
+        ],
+        // adicionando o atributo para a tag body
+        bodyAttrs: {
+          class: 'bg-gray-400'
+        }
+    }
+  },
   data() {
     return {
-      services: []
-    }
+      services: [],
+      title: '',
+    };
   },
   // executado depois que o componente é criado
   async fetch() {
-    this.services = await this.$axios.$get('https://jsonplaceholder.typicode.com/users?_limit=10')
+    this.services = await this.$axios.$get(
+      "https://jsonplaceholder.typicode.com/users?_limit=10"
+    );
   },
-}
+  created() {
+    this.getTitle();
+  },
+  methods: {
+    getTitle() {
+      setTimeout(() => {
+        this.title = "Serviços"
+      }, 3000);
+    }
+  },
+};
 </script>
